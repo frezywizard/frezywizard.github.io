0ec05f4ae89058c930c7a7c108fe696f5bbe20da
frezywizard <77693945+frezywizard@users.noreply.github.com>
2023-05-11 22:22:14 +0100

    Update personalWebsite.js

--- personalWebsite.js
+++ personalWebsite.js
@@ -1,26 +1,60 @@
-let aboutMe = document.getElementById("aboutMe");
-let resume = document.getElementById("resume");
-let ph1 = document.getElementById("PlH1");
-let ph2 = document.getElementById("PlH2");
+
+
+
+
 
 let allThemFacts = document.querySelector("#sideBarBtns");
 
-allThemFacts.addEventListener("click", changeSizeAndTextabtme);
 
-aboutMe.addEventListener("click", changeSizeAndTextabtme);
-resume.addEventListener("click", changeSizeAndTextresume);
-ph1.addEventListener("click", changeSizeAndTextph1);
-ph2.addEventListener("click", changeSizeAndTextph2);
+document.addEventListener("DOMContentLoaded", function() {
+    var aboutMe = document.getElementById("aboutMe");
+    var resume = document.getElementById("resume");
+    var ph1 = document.getElementById("PlH1");
+    var ph2 = document.getElementById("PlH2");
+    var originalText = aboutMe.textContent;
 
-function changeSizeAndTextabtme(){
-    aboutMe.style.color = "red";
-}
-function changeSizeAndTextresume(){
-    resume.style.color = "red";
-}
-function changeSizeAndTextph1(){
-    ph1.style.color = "red";
-}
-function changeSizeAndTextph2(){
-    ph2.style.color = "red";
-}
+
+    aboutMe.addEventListener("click", function() {
+        aboutMe.classList.remove("selected");
+        aboutMe.classList.add("selected");
+        changeSizeAndTextabtme();
+    });
+
+    resume.addEventListener("click", function() {
+        resume.classList.remove("selected");
+        resume.classList.add("selected");
+        changeSizeAndTextresume();
+    });
+
+    ph1.addEventListener("click", function() {
+        ph1.classList.remove("selected");
+        ph1.classList.add("selected");
+        changeSizeAndTextph1();
+    });
+
+    ph2.addEventListener("click", function() {
+        ph2.classList.remove("selected");
+        ph2.classList.add("selected");
+        changeSizeAndTextph2();
+    });
+
+    function changeTextBack() {
+        if (!aboutMe.contains(event.target)) {
+          aboutMe.textContent = originalText;
+        }
+      }
+      
+
+    function changeText() {
+        aboutMe.textContent = "Add content here soon";
+      }
+      
+      if (aboutMe.matches(":focus")) {
+        changeText();
+      }
+      
+      aboutMe.addEventListener("click", changeText);
+      document.addEventListener("click", changeTextBack);
+});
+
+
